951aefac67497819afc42927b65884623b128f49
Malashko17 <pawel.malashko@yandex.ru>
2023-06-04 17:02:58 +0300

    Commit all tests №6

--- conftest.py
+++ conftest.py
@@ -16,10 +16,10 @@ all_fields = (By.CLASS_NAME, 'form-control')
 @pytest.fixture(scope='function')
 def driver():
     options = Options()
-    # options.add_argument('--no-sandbox')  # bypass OS security model
-    # options.add_argument('--disable-dev-shm-usage')  # overcome limited resource problems
-    # options.add_argument('start-maximized')
-    # options.add_argument('--headless')
+    options.add_argument('--no-sandbox')  # bypass OS security model
+    options.add_argument('--disable-dev-shm-usage')  # overcome limited resource problems
+    options.add_argument('start-maximized')
+    options.add_argument('--headless')
     file_path = os.path.join(os.path.dirname(__file__), 'info_session')
     options.add_argument(f'user-data-dir={file_path}')
     chrome_driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
