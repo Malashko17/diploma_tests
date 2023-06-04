b2eb5bab784e552b8935eeeec56bb279bc5bafb4
Malashko17 <pawel.malashko@yandex.ru>
2023-06-04 13:20:15 +0300

    Commit all tests №2

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 email = 'pawel.malashko@yandex.ru'
 first_name = 'Pavel'
 last_name = 'Malashko'
-password = '123'
-new_password = '321'
+password = '321'
+new_password = '123'
 
 motivation_note = 'Never give up. ' \
                   'Never put off till tomorrow what you can do today. ' \
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -47,16 +47,16 @@ class TestLogin:
         login_page.open_login_page()
         assert login_page.check_that_forget_password_link_presents_on_the_login_page()
 
-    @allure.severity(allure.severity_level.CRITICAL)
-    @allure.story('Testing the login page')
-    @pytest.mark.enter_with_a_new_password
-    # @pytest.mark.skip('Run only at the end of testing')
-    def test_try_to_enter_with_a_new_password(self,driver):
-        login_page = LoginPage(driver)
-        login_page.open_login_page()
-        assert login_page.check_that_remember_me_box_is_selected()
-        login_page.enter_login_data(email=settings.email, password=settings.new_password)
-        home_page = HomePage(driver)
-        assert home_page.check_that_username_is_displayed_in_the_welcome_block
+    # @allure.severity(allure.severity_level.CRITICAL)
+    # @allure.story('Testing the login page')
+    # @pytest.mark.enter_with_a_new_password
+    # # @pytest.mark.skip('Run only at the end of testing')
+    # def test_try_to_enter_with_a_new_password(self, driver):
+    #     login_page = LoginPage(driver)
+    #     login_page.open_login_page()
+    #     assert login_page.check_that_remember_me_box_is_selected()
+    #     login_page.enter_login_data(email=settings.email, password=settings.new_password)
+    #     home_page = HomePage(driver)
+    #     assert home_page.check_that_username_is_displayed_in_the_welcome_block
 
 
